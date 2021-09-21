45d2a005d8cbcf28909613a3d6c7fa346eab1982
jawadkowshik <90059496+jawadkowshik@users.noreply.github.com>
2021-09-21 22:53:21 +0600

    Delete deliver_2/customer directory

--- deliver_2/customer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem , Category , OrderModel, Customer
-
-admin.site.register(MenuItem)
-admin.site.register(Category)
-admin.site.register(OrderModel)
-admin.site.register(Customer)
